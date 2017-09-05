62d8914e9d4d5b9020dc725944a6533fdf93691d
Akshay Mathur <avi.mathur.engg@gmail.com>
2017-09-06 01:12:34 +0530

    Adding moves for Queen
    Adding moves for queen

--- src/thelearninggames/chess/pieces/Queen.java
+++ src/thelearninggames/chess/pieces/Queen.java
@@ -28,6 +28,63 @@ public class Queen implements Piece {
     @Override
     public ArrayList<Integer> getValidMoves(int row , int col, boolean isToEmpty) {
         ArrayList<Integer> moves = new ArrayList<>();
+        Direction d = new Direction(row, col);
+        int temp = d.UPRIGHT;
+        int i = 2;
+        while(temp != d.CENTER){
+            moves.add(temp);
+            temp = d.getUPRIGHT(i);
+            i++;
+        }
+        temp = d.UPLEFT;
+        i = 2;
+        while(temp != d.CENTER){
+            moves.add(temp);
+            temp = d.getUPLEFT(i);
+            i++;
+        }
+        temp = d.DOWNRIGHT;
+        i = 2;
+        while(temp != d.CENTER){
+            moves.add(temp);
+            temp = d.getDOWNRIGHT(i);
+            i++;
+        }
+        temp = d.DOWNLEFT;
+        i = 2;
+        while(temp != d.CENTER){
+            moves.add(temp);
+            temp = d.getDOWNLEFT(i);
+            i++;
+        }
+        temp = d.RIGHT;
+        i = 2;
+        while(temp != d.CENTER){
+            moves.add(temp);
+            temp = d.getRIGHT(i);
+            i++;
+        }
+        temp = d.LEFT;
+        i = 2;
+        while(temp != d.CENTER){
+            moves.add(temp);
+            temp = d.getLEFT(i);
+            i++;
+        }
+        temp = d.DOWN;
+        i = 2;
+        while(temp != d.CENTER){
+            moves.add(temp);
+            temp = d.getDOWN(i);
+            i++;
+        }
+        temp = d.UP;
+        i = 2;
+        while(temp != d.CENTER){
+            moves.add(temp);
+            temp = d.getUP(i);
+            i++;
+        }
         return moves;
     }
 
